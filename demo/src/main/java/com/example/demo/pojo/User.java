60e222561c49f7b5fb85934e712c80ca46b249c2
szksbqys <1325646989@qq.com>
2023-05-01 19:02:04 +0800

    提交到本地仓库

---
+++ demo/src/main/java/com/example/demo/pojo/User.java
@@ -0,0 +1,13 @@
+package com.example.demo.pojo;
+
+import lombok.Data;
+
+/**
+ * author HP
+ * date 2023/5/1 19:00
+ */
+@Data
+public class User {
+    private String userName;
+    private String passWord;
+}
